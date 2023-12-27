6c5a0f39335585c284b198f97456b2ff63f78b13
zubaermahmud <zubaer.mahmud31@gmail.com>
2023-12-28 01:28:47 +0600

    committing

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 void main() {
   runApp(const MyApp());
 }
