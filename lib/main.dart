71597eb094f5775372b4bea4caa6c2a20583940c
zubaermahmud <zubaer.mahmud31@gmail.com>
2023-12-28 00:11:27 +0600

    adding dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 void main() {
   runApp(const MyApp());
 }
