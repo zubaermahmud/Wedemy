66f6a618c99fea266909e75fed9eed4ba7aab80a
zubaermahmud <zubaer.mahmud31@gmail.com>
2024-01-05 12:14:00 +0600

    Shihab

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:wedemy/sign up.dart';
-import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'sign up.dart'; // Import the file where SignUpPage is defined
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(SignUpApp());
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //home: AuthListener(),
+      title: 'Flutter Firebase Auth Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignUpPage(), // Set SignUpPage as the initial route
     );
   }
 }
