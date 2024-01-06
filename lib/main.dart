c01ed853e5ab7b37266ad9dacc9b632c2a96e6c4
zubaermahmud <zubaer.mahmud31@gmail.com>
2024-01-07 01:05:08 +0600

    shihab

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'sign up.dart';
+import 'signup.dart';
+import 'login.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -20,7 +21,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignUpPage()
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WeDemy'),
+        backgroundColor: Colors.blue[800],
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Welcome Students",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                ),
+              ),
+
+              Text(
+                "Your future depends on You",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.blueGrey,
+                ),
+              ),
+              Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage("images/img.png")
+                      )
+                  )
+              ),
+              Column(
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed:(){
+                      Navigator.push(context,MaterialPageRoute( builder: (context) => login()));
+                    },
+                    color:Colors.blue[800],
+                    shape: RoundedRectangleBorder(
+                        side:BorderSide(
+                          color:Colors.blue,
+                        ),
+                        borderRadius:BorderRadius.circular(50)
+
+                    ),
+                    child:Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 25,
+
+                        color: Colors.white,
+
+                      ),
+                    ) ,
+                  )
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed:(){
+                      Navigator.push(context,MaterialPageRoute( builder: (context) => SignupPage()));
+                    },
+                    color:Colors.blue[800],
+                    shape: RoundedRectangleBorder(
+                        side:BorderSide(
+                          color:Colors.blue,
+                        ),
+                        borderRadius:BorderRadius.circular(50)
+
+                    ),
+                    child:Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ) ,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
