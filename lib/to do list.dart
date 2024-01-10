3a2bfd85b10c0632f04e5bd488c0081cf143878f
orik01556340144 <labibfarhan285@gmail.com>
2024-01-10 22:56:29 +0600

    to do list task add

--- lib/to do list.dart
+++ lib/to do list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wedemy/constants/colors.dart';
+import 'package:wedemy/to do item.dart';
 
 class todolist extends StatefulWidget {
   const todolist({super.key});
@@ -8,14 +10,118 @@ class todolist extends StatefulWidget {
 }
 
 class _todolistState extends State<todolist> {
+  final todolist = ToDo.todoList();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: tdBGColor,
       appBar: AppBar(
         title: Text('WeDemy'),
         backgroundColor: Colors.blue[800],
         foregroundColor: Colors.white,
+        elevation: 0,
         centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 6.0, left: 12.0, right: 12.0, bottom: 5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.menu,
+                        color: tdBlack,
+                        size: 40,
+                      ),
+                      Icon(
+                        Icons.account_circle,
+                        color: tdBlack,
+                        size: 50,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 12.0,
+                    left: 17,
+                    right: 17,
+                    bottom: 30,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(0),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: tdBlack,
+                          size: 20,
+                        ),
+                        prefixIconConstraints: BoxConstraints(
+                          maxHeight: 25,
+                          minWidth: 25,
+                        ),
+                        border: InputBorder.none,
+                        hintText: 'Search',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 50, left: 30),
+                        child: Text(
+                          'ALL TOdOs',
+                          style: (TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          )),
+                        ),
+                      ),
+                      for (ToDo todoo in todolist)
+                        ToDoItem(
+                          todo: todoo,
+                          //onToDoChanged: _handleToDoChange,
+                          //onDeleteItem: _deleteToDoItem,
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        bottom: 20,
+                        right: 20,
+                        left: 20,
+                      ),
+                      decoration: BoxDecoration(
+
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
